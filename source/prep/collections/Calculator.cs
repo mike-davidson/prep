c1c7efe812bcb44a8741e94f49986946ea10f730
git_name <git_email>
2012-12-05 14:42:52 -0600

    Pushing new changes

--- source/prep/collections/Calculator.cs
+++ source/prep/collections/Calculator.cs
@@ -1,9 +1,14 @@
-﻿namespace prep.collections
+﻿using System;
+
+namespace prep.collections
 {
   public class Calculator
   {
     public int add(int i, int i1)
     {
+        if ((i < 0) || (i1 < 0))
+            throw new ArgumentException();
+
         return i + i1;
     }
   }
