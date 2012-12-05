b9bfe7224d9f9755b6bd8a98912d14a7aeb860f6
mike-davidson <mdavidson@deltadentalmi.com>
2012-12-05 15:41:09 -0500

    Pushing new changes

--- source/prep/collections/Calculator.cs
+++ source/prep/collections/Calculator.cs
@@ -1,9 +1,12 @@
-﻿namespace prep.collections
+﻿using System;
+namespace prep.collections
 {
   public class Calculator
   {
     public int add(int i, int i1)
     {
+        if (i > 0 || i1 > 0)
+            throw new ArgumentException();
         return i + i1;
     }
   }
