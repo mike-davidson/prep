027885bc024f713198f3fa5a62caa27ffbc73f46
jgiannola <jgiannola@deltadentalmi.com>
2012-12-05 16:02:42 -0500

    Pushing new changes

--- source/prep/collections/Calculator.cs
+++ source/prep/collections/Calculator.cs
@@ -1,15 +1,23 @@
 ﻿using System;
+using System.Data;
 
 namespace prep.collections
 {
   public class Calculator
   {
+    IDbConnection connection;
     public int add(int i, int i1)
     {
+        connection.Open();
         if ((i < 0) || (i1 < 0))
             throw new ArgumentException();
 
         return i + i1;
     }
+    public Calculator(IDbConnection connection)
+    {
+        this.connection = connection;
+    }
+     
   }
 }
